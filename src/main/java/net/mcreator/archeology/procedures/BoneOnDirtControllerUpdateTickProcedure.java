8621153f0c52961cd278280bed2aab12e43301bc
SJmod <Sreve@192.168.0.101>
2021-12-13 02:28:16 +0000

    - Tweaked bone dirt patch a bit, added 20% chance for mega ore deposit to spawn under bone dirt patch. This spawn percentage will be lower as we add more things to spawn under the patch as it is quite OP.

--- src/main/java/net/mcreator/archeology/procedures/BoneOnDirtControllerUpdateTickProcedure.java
+++ src/main/java/net/mcreator/archeology/procedures/BoneOnDirtControllerUpdateTickProcedure.java
@@ -41,10 +41,15 @@ public class BoneOnDirtControllerUpdateTickProcedure {
 		double sx = 0;
 		double sy = 0;
 		double sz = 0;
+		boolean orebelow = false;
+		orebelow = (boolean) (false);
+		if ((Math.random() < 0.2)) {
+			orebelow = (boolean) (true);
+		}
 		sx = (double) (-3);
 		for (int index0 = 0; index0 < (int) (6); index0++) {
-			sy = (double) (-1);
-			for (int index1 = 0; index1 < (int) (2); index1++) {
+			sy = (double) (-3);
+			for (int index1 = 0; index1 < (int) (6); index1++) {
 				sz = (double) (-3);
 				for (int index2 = 0; index2 < (int) (6); index2++) {
 					if (((!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz)))).getBlock() == Blocks.AIR))
@@ -56,20 +61,37 @@ public class BoneOnDirtControllerUpdateTickProcedure {
 										.getMaterial() == net.minecraft.block.material.Material.ROCK)
 										|| ((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
 												.getMaterial() == net.minecraft.block.material.Material.ORGANIC)))) {
-							if ((Math.random() < 0.6)) {
-								{
-									BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
-									BlockState _bs = BonesondirtBlock.block.getDefaultState();
-									BlockState _bso = world.getBlockState(_bp);
-									for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-										Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
-										if (_property != null && _bs.get(_property) != null)
-											try {
-												_bs = _bs.with(_property, (Comparable) entry.getValue());
-											} catch (Exception e) {
-											}
+							if ((Math.random() < 0.8)) {
+								if ((Math.random() < 0.75)) {
+									{
+										BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+										BlockState _bs = BonesondirtBlock.block.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										world.setBlockState(_bp, _bs, 3);
 									}
-									world.setBlockState(_bp, _bs, 3);
+								} else {
+									{
+										BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+										BlockState _bs = Blocks.COARSE_DIRT.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										world.setBlockState(_bp, _bs, 3);
+									}
 								}
 							}
 						}
@@ -93,7 +115,7 @@ public class BoneOnDirtControllerUpdateTickProcedure {
 											.getBlock() == BoneOnDirtControllerBlock.block))))) {
 						if ((((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
 								.getMaterial() == net.minecraft.block.material.Material.EARTH)
-								|| (((world.getBlockState(new BlockPos((int) (x + sx), (int) 0, (int) (z + sz))))
+								|| (((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
 										.getMaterial() == net.minecraft.block.material.Material.ROCK)
 										|| ((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
 												.getMaterial() == net.minecraft.block.material.Material.ORGANIC)))) {
@@ -152,6 +174,210 @@ public class BoneOnDirtControllerUpdateTickProcedure {
 				sy = (double) (sy + 1);
 			}
 			sx = (double) (sx + 1);
+		}
+		sx = (double) (-7);
+		for (int index6 = 0; index6 < (int) (14); index6++) {
+			sy = (double) (-7);
+			for (int index7 = 0; index7 < (int) (14); index7++) {
+				sz = (double) (-7);
+				for (int index8 = 0; index8 < (int) (14); index8++) {
+					if (((!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz)))).getBlock() == Blocks.AIR))
+							&& ((!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) sy)))
+									.getBlock() == BonesondirtBlock.block))
+									&& (!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+											.getBlock() == BoneOnDirtControllerBlock.block))))) {
+						if ((((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+								.getMaterial() == net.minecraft.block.material.Material.EARTH)
+								|| (((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+										.getMaterial() == net.minecraft.block.material.Material.ROCK)
+										|| ((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+												.getMaterial() == net.minecraft.block.material.Material.ORGANIC)))) {
+							if ((Math.random() < 0.15)) {
+								if ((Math.random() < 0.1)) {
+									{
+										BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+										BlockState _bs = BonesondirtBlock.block.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										world.setBlockState(_bp, _bs, 3);
+									}
+								} else if ((Math.random() < 0.3)) {
+									{
+										BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+										BlockState _bs = Blocks.COARSE_DIRT.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										world.setBlockState(_bp, _bs, 3);
+									}
+								} else {
+									{
+										BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+										BlockState _bs = Blocks.GRASS_PATH.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										world.setBlockState(_bp, _bs, 3);
+									}
+								}
+							}
+						}
+					}
+					sz = (double) (sz + 1);
+				}
+				sy = (double) (sy + 1);
+			}
+			sx = (double) (sx + 1);
+		}
+		if ((orebelow == (true))) {
+			sx = (double) (-7);
+			for (int index9 = 0; index9 < (int) (14); index9++) {
+				sy = (double) (-10);
+				for (int index10 = 0; index10 < (int) (14); index10++) {
+					sz = (double) (-7);
+					for (int index11 = 0; index11 < (int) (14); index11++) {
+						if (((!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz)))).getBlock() == Blocks.AIR))
+								&& ((!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) sy)))
+										.getBlock() == BonesondirtBlock.block))
+										&& (!((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+												.getBlock() == BoneOnDirtControllerBlock.block))))) {
+							if (((world.getBlockState(new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz))))
+									.getMaterial() == net.minecraft.block.material.Material.ROCK)) {
+								if ((Math.random() < 0.5)) {
+									if ((Math.random() < 0.6)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.COAL_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else if ((Math.random() < 0.5)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.IRON_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else if ((Math.random() < 0.4)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.REDSTONE_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else if ((Math.random() < 0.4)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.GOLD_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else if ((Math.random() < 0.3)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.LAPIS_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else if ((Math.random() < 0.3)) {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.EMERALD_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									} else {
+										{
+											BlockPos _bp = new BlockPos((int) (x + sx), (int) (y + sy), (int) (z + sz));
+											BlockState _bs = Blocks.DIAMOND_ORE.getDefaultState();
+											BlockState _bso = world.getBlockState(_bp);
+											for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+												Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+												if (_property != null && _bs.get(_property) != null)
+													try {
+														_bs = _bs.with(_property, (Comparable) entry.getValue());
+													} catch (Exception e) {
+													}
+											}
+											world.setBlockState(_bp, _bs, 3);
+										}
+									}
+								}
+							}
+						}
+						sz = (double) (sz + 1);
+					}
+					sy = (double) (sy + 1);
+				}
+				sx = (double) (sx + 1);
+			}
 		}
 		{
 			BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
